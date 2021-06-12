683916dd71e66b83b4b02c8bf54ed49b64443349
Ray Martinez <rmart404@stu.southtexascollege.edu>
2021-06-12 15:12:26 -0500

    Finished

---
+++ Manager.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Employees
+{
+  class Manager : Employee
+  {
+    public int StockOptions { get; set; }
+
+
+    public Manager(string fullName, int age, int empId, float currPay, string ssn, int numbOfOpts) : base(fullName, age, empId, currPay, ssn, EmployeePayTypeEnum.Salaried)
+    {
+      StockOptions = numbOfOpts;
+    }
+
+    public override void GiveBonus(float amount)
+    {
+      base.GiveBonus(amount);
+      Random r = new Random();
+      StockOptions += r.Next(500);
+    }
+    public override void DisplayStats()
+    {
+      base.DisplayStats();
+      Console.WriteLine("Number of Sales {0}", StockOptions);
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Employees;
+
+Manager chucky = new Manager("Chucky", 50, 92, 100_000, "333-23-2322", 9_000);
+chucky.GiveBonus(300);
+chucky.DisplayStats();
+Console.WriteLine();
+
+SalesPerson fran = new SalesPerson("Fran", 43, 93, 3_000, "333-23-2322", 31);
+fran.GiveBonus(200);
+fran.DisplayStats();
+Console.ReadLine();
+
+//Employee x = new Employee();
